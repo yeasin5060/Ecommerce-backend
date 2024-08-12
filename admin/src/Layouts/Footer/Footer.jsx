f197483343d5d649edb8a71c6f92d0f18bc1059d
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-08-12 10:03:12 +0600

    add admin dashboard footer

--- admin/src/Layouts/Footer/Footer.jsx
+++ admin/src/Layouts/Footer/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Heading from '../../utils/Heading/Heading';
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section id='footer'>
+      <div className='container'>
+        <div className='footer_wrapper'>
+          <div className='footer_wrapper_copyright_box'>
+              <Heading level="h4" text="Copyright © 2024. All rights are reserved" className="footer_wrapper_copyright_contant"/>
+          </div>
+        </div>
+      </div>
+   </section>
   )
 }
 
